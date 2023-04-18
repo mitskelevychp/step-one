f92d8819113b4bbf2b8fa47a198d3d1daefce1c9
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2023-04-18 21:52:58 +0300

    Gallery almost finish - before showing big pictures after :hover + have to think about the grid

--- scripts/script-gallery.js
+++ scripts/script-gallery.js
@@ -1,27 +1,48 @@
 /* BLOCK 8 - GALLERY */
+var grid = document.querySelector(".grid");
+var msnry = new Masonry(grid, {
+  // options
+  gutter: 18,
+  horizontalOrder: true,
+});
+
+//
+// button
 let loadButton = document.querySelector(".gallery__load-button");
+let galleryPictures = document.querySelectorAll(".grid-item");
 let loadButtonImg = document.querySelector(".gallery__load-button-img");
 let loadButtonText = document.querySelector(".gallery__load-button-text");
 
 loadButton.addEventListener("click", showLoadProcess);
 
-// // someFunction - show pictures after a click
-// function showLoadProcess() {
-//   show(loadButton, loadButtonImg, loadButtonText, 2000, someFunction);
-// }
-
+// someFunction - show pictures after a click using a pattern "script-work-examples.js"
 function showLoadProcess() {
-  setTimeout(startProcess, 0);
-  function startProcess() {
-    loadButton.classList.add("button-loading", "button-loading::after");
-    loadButtonImg.classList.add("button-loading__img");
-    loadButtonText.classList.add("button-loading__text");
-  }
+  show(loadButton, loadButtonImg, loadButtonText, 2000, someFunction);
+}
 
-  setTimeout(stopProcess, 2000);
-  function stopProcess() {
-    loadButton.classList.remove("button-loading", "button-loading::after");
-    loadButtonImg.classList.remove("button-loading__img");
-    loadButtonText.classList.remove("button-loading__text");
+function someFunction() {
+  function func() {
+    let elems = [];
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < 1; i++) {
+      let elem = getItemElement();
+      fragment.appendChild(elem);
+      elems.push(elem);
+    }
+    // append elements to container
+    grid.appendChild(fragment);
+    // add and lay out newly appended elements
+    msnry.appended(elems);
+
+    function getItemElement() {
+      let elem = document.createElement("div");
+      let rand = Math.random();
+      let randPictures = rand > 0.7 ? 1 : rand > 0.3 ? 3 : rand > 0 ? 5 : "";
+      let img = `<img src="./images/other/gallery-of-best-images-${randPictures}.jpg" alt="" />`;
+      elem.className = "grid-item";
+      elem.insertAdjacentHTML("afterbegin", img);
+      return elem;
+    }
   }
+  func();
 }
